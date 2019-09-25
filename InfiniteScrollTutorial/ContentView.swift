54af6154ca2bd9d70b0d871228b6f0cccf122599
Ephraim Nartey <narteyephraim@gmail.com>
2019-09-25 16:27:34 +0000

    Initial Commit

---
+++ InfiniteScrollTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfiniteScrollTutorial
+//
+//  Created by mac on 25/09/2019.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
