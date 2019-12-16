fb6447531cca76976b3ade809db751ac49c6a477
Ephraim Nartey <narteyephraim@gmail.com>
2019-12-16 11:21:02 +0000

    minor refactor

--- InfiniteScrollTutorial/InfiniteScrollView.swift
+++ InfiniteScrollTutorial/InfiniteScrollView.swift
@@ -9,37 +9,41 @@
 import SwiftUI
 
 struct InfiniteScrollView: View {
+    
 
-  // an ObservedObject object which holds the array we populate our List with.
-  @ObservedObject var myList = ObservableArray<Data>(array: [Data(value: "Infinite Scroll", id: 0)])
-    
-  //initialize ViewModel class which we will use to requests for data
-  let viewModel = InfiniteScrollViewModel()
-  
-  var body: some View {
-    
-    self.viewModel.viewUpdate = self
-    let enumeratedList = self.myList.array.enumerated().map({ $0 })
-    return List (enumeratedList, id: \.1.self.id) { (index,listItem) in
-        // set data value into a Text View
-        Text(listItem.value).onAppear(perform: {
+    // an ObservedObject object which holds the array we populate our List with.
+    @ObservedObject var myList = ObservableArray<Data>(array: [Data(value: "Infinite Scroll", id: 0)])
         
-            let count = self.myList.array.count
+    //initialize ViewModel class which we will use to requests for data
+    let viewModel = InfiniteScrollViewModel()
 
-            /*
-            *Whenever a view appears we check if the view appearing is the last
-            *view and get more items if it is the last view.
-            *In real cases you might need to increase the offset (which is in this case 1)
-            *to accommodate the delay while trying to reach the *server for more data
-            */
-            if index == count-1 {
-              // request for more data since last row has being reached
-              self.viewModel.getNewItems(currentListSize: count)
-            }
-            
-          })
+    init() {
+        // pass viewupdateprotocol to viewmodel
+        self.viewModel.viewUpdate = self
     }
-  }
+    
+    var body: some View {
+        
+        List (self.myList.array.enumerated().map({ $0 }), id: \.1.self.id) { (index,listItem) in
+            // set data value into a Text View
+            Text(listItem.value).onAppear(perform: {
+            
+                let count = self.myList.array.count
+
+                /*
+                *Whenever a view appears we check if the view appearing is the last
+                *view and get more items if it is the last view.
+                *In real cases you might need to increase the offset (which is in this case 1)
+                *to accommodate the delay while trying to reach the *server for more data
+                */
+                if index == count-1 {
+                  // request for more data since last row has being reached
+                  self.viewModel.getNewItems(currentListSize: count)
+                }
+                
+              })
+        }
+    }
 }
 
 extension InfiniteScrollView: ViewUpdateProtocol{
